6f2793b3b9778e7927c938431a50b67e2c5a31dd
Ben Governali <bgovern93@gmail.com>
2019-10-10 14:02:36 -0400

    added auth config and updated App.js for auth0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import "./App.css";
+import NavBar from "./components/NavBar";
+import { useAuth0 } from "./react-auth0-wrapper";
 
 function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <div>HEADER</div>
+      <header>
+        <NavBar />
       </header>
     </div>
   );
